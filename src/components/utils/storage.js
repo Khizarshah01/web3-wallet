e8cb095a537ef7c57a7dbebb9d4a69852d2a61e7
Khizarshah01 <khizar.shah010@gmail.com>
2025-04-23 12:44:59 +0530

    helper function for set and get token

---
+++ src/components/utils/storage.js
@@ -0,0 +1,9 @@
+export const getUserVisibleTokens = () => {
+    const saved = localStorage.getItem('visibleTokens');
+    return saved ? JSON.parse(saved) : ['ETH']; // default visible
+  };
+  
+  export const setUserVisibleTokens = (symbols) => {
+    localStorage.setItem('visibleTokens', JSON.stringify(symbols));
+  };
+  
