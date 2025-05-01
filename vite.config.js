376431548e33031f567963f57663f8587183ba43
Khizarshah01 <khizar.shah010@gmail.com>
2025-05-01 14:42:46 +0530

    fix the chunk size limit

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,15 @@ export default defineConfig({
     topLevelAwait(), 
   ],
   build: {
+    rollupOptions: {
+      output:{
+          manualChunks(id) {
+              if (id.includes('node_modules')) {
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+          }
+      }
+  },
     target: "es2022",
   }
 })
