f86e61ce7ea041aaa60493ad632ea81c16036f79
Khizarshah01 <khizar.shah010@gmail.com>
2025-05-01 14:36:00 +0530

    fix config for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import wasm from 'vite-plugin-wasm';
-// https://vite.dev/config/
+import topLevelAwait from "vite-plugin-top-level-await";
+
 export default defineConfig({
-  plugins: [react(),tailwindcss(),nodePolyfills(),wasm()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills(),
+    wasm(),
+    topLevelAwait(), 
+  ],
+  build: {
+    target: "es2022",
+  }
 })
